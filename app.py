91584203f1bb3f8896359c384c8cc65d19cb3eb7
42bergoblin <42bergoblin@gmail.com>
2024-06-15 17:42:38 +0900

    error fix

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ import matplotlib.pyplot as plt
 import io
 import base64
 from PIL import Image
-from bs4 import BeautifulSoup
-import pathlib
-import shutil
 
 im = Image.open("favicon.ico")
 st.set_page_config(
@@ -17,34 +14,6 @@ st.set_page_config(
     page_icon=im,
     )
 
-# Google Analytics 트래킹 코드 삽입
-GA_ID = "google_analytics"
-ga_tracking_code = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BKEQRCKBE6"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-BKEQRCKBE6');
-</script>
-"""
-
-
-def inject_ga():
-    
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
-        else:
-            shutil.copy(index_path, bck_index)  
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + ga_tracking_code)
-        index_path.write_text(new_html)
 
 
 
@@ -318,5 +287,3 @@ st.markdown("""
 
 if st.session_state.modal_clicked:
     show_modal()
-
-inject_ga()
