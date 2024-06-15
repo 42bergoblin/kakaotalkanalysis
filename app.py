08f38d68901d824ebbd80314cf68f39026a5ddff
42bergoblin <42bergoblin@gmail.com>
2024-06-15 17:39:28 +0900

    dasd

---
+++ app.py
@@ -0,0 +1,322 @@
+import streamlit as st
+import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import module
+# import dummy_module as module
+import matplotlib.pyplot as plt
+import io
+import base64
+from PIL import Image
+from bs4 import BeautifulSoup
+import pathlib
+import shutil
+
+im = Image.open("favicon.ico")
+st.set_page_config(
+    page_title="카카오톡 대화 분석 서비스",
+    page_icon=im,
+    )
+
+# Google Analytics 트래킹 코드 삽입
+GA_ID = "google_analytics"
+ga_tracking_code = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BKEQRCKBE6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BKEQRCKBE6');
+</script>
+"""
+
+
+def inject_ga():
+    
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID): 
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  
+        else:
+            shutil.copy(index_path, bck_index)  
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + ga_tracking_code)
+        index_path.write_text(new_html)
+
+
+
+# 로컬에서만 .env 파일에서 API 키 가져오기
+if os.getenv("IS_STREAMLIT_CLOUD") != "true":
+    from dotenv import load_dotenv
+    load_dotenv() 
+    api_key = os.getenv("OPENAI_API_KEY")
+
+# Mainpage UI
+st.title('카카오톡 대화 분석 서비스')
+st.markdown('''
+가장 최근 대화부터 최대 10만자까지의 대화를 분석합니다.  
+이를 통해 나와 상대방의 대화 습관, 심리, 추억 등을 알 수 있습니다.  
+''')
+custom_css = """
+    <style>
+        #custom-text {
+            background-color: #DFF0D8;
+            color: #3C763D;
+            font-size: 14px;
+            padding-top: 10px;
+            padding-bottom: 10px;
+            padding-left: 14px;
+            padding-right: 20px;
+            border-radius: 5px;
+            margin-top: -20px;
+            margin-bottom: 30px;
+        }
+    </style>
+"""
+st.markdown(custom_css, unsafe_allow_html=True)
+st.markdown('<p id="custom-text">이 서비스는 카톡 내용을 저장하지 않으니 안심하세요! GPT에게 분석을 요청할 때도 개인정보는 암호화됩니다.</p>', unsafe_allow_html=True)
+
+
+
+# 세션 상태 초기화
+if 'clicked_buttons' not in st.session_state:
+    st.session_state.clicked_buttons = []
+if 'selected_button' not in st.session_state:
+    st.session_state.selected_button = None
+if 'results' not in st.session_state:
+    st.session_state.results = {}
+if 'modal_clicked' not in st.session_state:
+    st.session_state.modal_clicked = False
+if 'modal_title' not in st.session_state:
+    st.session_state.modal_title = "TITLE"
+if 'cleaned_content' not in st.session_state:
+    st.session_state.cleaned_content = None
+if 'combined_chunks' not in st.session_state:
+    st.session_state.combined_chunks = ""
+if 'combined_responses' not in st.session_state:
+    st.session_state.combined_responses = ""
+if 'final_result' not in st.session_state:
+    st.session_state.final_result = ""
+if 'file_uploaded' not in st.session_state:
+    st.session_state.file_uploaded = False
+if 'is_loading' not in st.session_state:
+    st.session_state.is_loading = False
+if 'uploaded_file' not in st.session_state:
+    st.session_state.uploaded_file = None
+if 'wordcloud_img' not in st.session_state:
+    st.session_state.wordcloud_img = None
+
+#img_data = None
+def create_wordcloud():
+    # 파일 읽기
+    file_content = st.session_state.uploaded_file.read().decode("utf-8")
+    
+    # 시간 정보 제거
+    try:
+        cleaned_content = module.group_chat_dialogs(file_content)
+    except Exception:
+        cleaned_content = file_content
+
+    chunks = module.split_text(cleaned_content)
+    st.session_state.cleaned_content = cleaned_content
+    st.session_state.combined_chunks = "\n\n".join(chunks)
+
+    words = module.preprocess_text_for_wordcloud(cleaned_content)
+    word_frequencies = module.get_word_frequencies(words)
+
+    module.generate_wordcloud(word_frequencies)
+    
+    # 워드클라우드를 이미지로 변환
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    img_data = base64.b64encode(buf.read()).decode('utf-8')
+    buf.close()
+
+    st.session_state.wordcloud_img = img_data
+
+def basic_analyze(combined_responses):
+
+
+
+    # GPT-4 API 요청 병렬 처리
+    responses = []
+    with ThreadPoolExecutor() as executor:
+        chunks = module.split_text(st.session_state.cleaned_content)
+        future_to_chunk = {executor.submit(module.gpt_request, chunk): chunk for chunk in chunks}
+        for future in as_completed(future_to_chunk):
+            try:
+                response = future.result()
+                responses.append(response)
+            except Exception as exc:
+                st.error(f"Chunk 처리 중 오류 발생: {exc}")
+
+    # 응답 통합
+    st.session_state.combined_responses = "\n\n".join(responses)
+
+    final_result = module.aggregate_responses(st.session_state.combined_responses)
+    st.session_state.final_result = final_result
+    print("basic analyze done")
+    def print_results():
+        st.write(f"{final_result}")
+        st.image(f"data:image/png;base64,{st.session_state.wordcloud_img}", use_column_width=True)
+    return print_results
+
+
+# 모달 함수 정의
+@st.experimental_dialog(" ", width="large")
+def show_modal():
+    st.markdown("Title" if st.session_state.modal_title is None else "# "+st.session_state.modal_title)
+    (button_name, explanation, process_function, source)  = st.session_state.selected_button
+
+    modal_content = "Content"
+
+    if st.session_state.wordcloud_img is None:
+        with st.spinner(f'워드클라우드를 생성중입니다!'):
+            create_wordcloud()
+
+    if button_name == "기본 분석" and button_name not in st.session_state.clicked_buttons:
+        st.image(f"data:image/png;base64,{st.session_state.wordcloud_img}", use_column_width=True)        
+
+    if button_name in st.session_state.clicked_buttons:
+        # 이미 실행한 결과가 있으면 그 결과를 모달에 표시
+        modal_content = st.session_state.results[button_name][1]
+        st.session_state.is_loading = False
+    else:
+        st.session_state.is_loading = True
+        with st.spinner(f'{button_name} 진행중입니다. 조금만 기다려주세요!'):
+            result = process_function(source)
+            st.session_state.results[button_name]=(explanation, result)
+            st.session_state.clicked_buttons.append(button_name)
+            modal_content = result
+            st.rerun()
+
+    if callable(modal_content):  # additional_result가 함수인지 확인
+        modal_content()
+    else:
+        st.write(f"{modal_content}")
+
+
+
+
+
+
+
+
+
+def handle_button_click(button):
+    st.session_state.modal_title = button[0]
+    st.session_state.selected_button = button
+    st.session_state.modal_clicked=True
+    # if not st.session_state.is_loading:
+    #     st.session_state.modal_clicked = True
+
+# 버튼들 정의
+available_buttons = [
+    ('기본 분석', "카카오톡 대화를 분석하여 관계 리포트를 뽑아드려요", basic_analyze, st.session_state.combined_responses),
+    ('감정 단어 분석하기', "둘 사이에 어떤 감정 단어가 가장 많이 오고 갔을까요?", module.emotion_donut, st.session_state.combined_responses),
+    ('월별 추억 돌아보기', "현생에 치여 잊고 살아왔던 둘만의 추억을 돌아봐요", module.monthly_event, st.session_state.combined_responses),
+    ('전생 관계 분석', "우린 전생에 어떤 사이길래 이렇게 다시 만났을까요?", module.analyze_past_life, st.session_state.combined_responses),
+    ('랩 가사 작성', "신나는 비트에서 느껴지는 우리의 힙한 관계!", module.write_rap_lyric, st.session_state.final_result),
+    ('기념일 생성', "선배 기념일 만들어주세요! 혹시.. 우리 추억도 같이??", module.create_anniversary, st.session_state.combined_responses),
+]
+
+# 파일 업로드   
+st.session_state.uploaded_file = st.file_uploader("카카오톡 채팅 내역 업로드", type="txt")
+if st.session_state.uploaded_file is not None and st.session_state.file_uploaded is False:
+    st.session_state.file_uploaded = True
+    handle_button_click(available_buttons[0])
+
+elif st.session_state.uploaded_file is None:
+    st.session_state.file_uploaded = False
+
+
+
+
+# 버튼 UI 구성 및 클릭 처리
+cols = st.columns(3)
+for idx, button in enumerate(available_buttons):
+    (button_name, explanation, process_function, use_response) = button
+    is_clicked = button_name in st.session_state.clicked_buttons
+    background_color = "#d3d3d3" if is_clicked else "#f0f0f0"
+    button_style = f"background-color: {background_color}; height: 200px; width: 100%; font-size: 20px; border: none;"
+    button_label = button_name if not is_clicked else f"{button_name}(완료)"
+    button_explanation = explanation if not is_clicked else "클릭해서 결과를 확인하세요"
+
+    with cols[idx % 3]:
+        def on_click_clicked():
+            print("trying : "+button_name)
+            
+
+        if st.button(f"### {button_label}\n {button_explanation}"):
+            if st.session_state.file_uploaded :
+                handle_button_click(button)
+            else:
+                st.toast("카카오톡 대화를 먼저 입력하세요")
+
+
+st.markdown("""
+    <style>
+    .centered-button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        bottom: 20px;
+        width: 100%;
+    }
+    .centered-button a {
+        background-color: #794BDB;
+        color: white;
+        padding: 14px 25px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        border-radius: 15px 15px 0px 15px;
+    }
+    .centered-button a:hover {
+        background-color: #6C43C4;
+    }
+    </style>
+    <div class="centered-button">
+        <a href="https://forms.gle/ysV2jzyU352BcmDx5" target="_blank">사용 후기 남기기 🥰</a>
+    </div>
+""", unsafe_allow_html=True)
+
+# CSS 스타일로 버튼 크기 조정
+st.markdown("""
+    <style>
+    div.stButton > button {
+        height: 200px;
+        width: 100%;
+        font-size: 20px;
+        font-weight: bold;
+
+    }
+    div[data-testid=stSpinner] {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+        div[data-testid=stToast] {
+        padding: 0 auto;
+        margin: 0 auto;
+        color: white;
+        background: #ff4b4b;
+        width: 20%;
+        min-width:400px;
+    }
+        
+    </style>
+
+    """, unsafe_allow_html=True)
+
+
+
+if st.session_state.modal_clicked:
+    show_modal()
+
+inject_ga()
---
+++ module.py
@@ -0,0 +1,513 @@
+from openai import OpenAI
+import json
+from streamlit_apexjs import st_apexcharts
+import streamlit as st
+import re
+import os
+from openai import OpenAI
+from collections import Counter, defaultdict
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+import os
+
+
+use_upstage = False
+
+
+# 로컬에서만 .env 파일에서 API 키 가져오기
+if os.getenv("IS_STREAMLIT_CLOUD") != "true":
+    from dotenv import load_dotenv
+    load_dotenv() 
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    api_key = os.getenv("UPSTAGE_API_KEY")
+
+def preprocess_text_for_wordcloud(text):
+    cleaned_text = re.sub(r'\[.*?\]', '', text)
+    cleaned_text = re.sub(r'\d{4}년 \d{1,2}월 \d{1,2}일 \w요일', '', cleaned_text)
+    cleaned_text = re.sub(r'\d{1,2}:\d{1,2}', '', cleaned_text)
+    cleaned_text = re.sub(r'\n', ' ', cleaned_text)
+    cleaned_text = re.sub(r'[^가-힣\s]', '', cleaned_text)
+    words = cleaned_text.split()
+
+    stop_words = set(['년', '월', '일', '오전', '오후', '시간', '이모티콘', '나', '너', '난', '아', '좀', '흠', '카카오페이머니는', '온오프라인', '가능해요'])
+
+    name_pattern = re.compile(r'\b[가-힣]{2,3}\b')
+    potential_names = [word for word in words if name_pattern.match(word)]
+    name_frequencies = Counter(potential_names)
+
+    common_names = {name for name, count in name_frequencies.items() if count > 10}
+    words = [word for word in words if word not in common_names and word not in stop_words and len(word) > 1]
+
+    return words
+
+
+
+DATE_PATTERN1 = r'(\d{4}년 \d{1,2}월 \d{1,2}일) ((오전|오후)?\s*\d{1,2}:\d{1,2})'   # 2023년 12월 12일 (오전) 4:24   
+DATE_PATTERN2 = r'(\d{4}. \d{1,2}. \d{1,2}(.)?) ((오전|오후)?\s*\d{1,2}:\d{1,2})'    # 2023. 10. 1(.) (오전) 4:24
+DATE_PATTERN3 = r'(\d{4}/\d{1,2}/\d{1,2}) (\d{1,2}:\d{1,2})'        # 2023/1/12 19:23
+DATE_PATTERN4 = r'(\d{4}-\d{1,2}-\d{1,2}) (\d{1,2}:\d{1,2}:\d{1,2})'        # 2024-03-15 14:05:17
+
+DATE_PATTERN_PC_DATE = r'(-*\s*\d{4}년 \d{1,2}월 \d{1,2}일 [월화수목금토일]\s*-*)'  # --------------- 2024년 4월 3일 수요일 ---------------
+DATE_PATTERN_PC_MSG = r'\[(.*)\] \[((오전|오후)? \d{1,2}:\d{2})\]' # [홍길동] [오후 10:49]
+
+
+def get_word_frequencies(words):
+    counter = Counter(words)
+    return counter
+
+def generate_wordcloud(word_frequencies):
+    wordcloud = WordCloud(width=800, height=600, background_color='white', font_path='assets/NanumSquareRoundR.ttf').generate_from_frequencies(word_frequencies)
+    plt.figure(figsize=(7, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+
+
+
+# obsolete
+def clean_text(text):
+    # 정규 표현식을 사용하여 날짜 형식을 남기고 시간 형식 제거
+    cleaned_text = re.sub(r'\d{4}/\d{2}/\d{2} \d{2}:\d{2}, ', '', text)
+    # 쉼표 뒤에 남아 있는 공백 제거
+    cleaned_text = re.sub(r',\s+', ', ', cleaned_text)
+    # 중복 공백 제거
+    cleaned_text = re.sub(r'\s{2,}', ' ', cleaned_text)
+    return cleaned_text
+
+def group_chat_dialogs(chat):
+    chat_lines = chat.strip().split('\n')
+    date_pattern = None
+
+        # find which pattern fits by trying various patterns
+    for line in chat_lines:
+        if date_pattern is None:
+            if re.match(DATE_PATTERN1, line):
+                date_pattern = DATE_PATTERN1
+                break
+            elif re.match(DATE_PATTERN2, line):
+                date_pattern = DATE_PATTERN2
+                break
+            elif re.match(DATE_PATTERN3, line):
+                date_pattern = DATE_PATTERN3
+                break
+            elif re.match(DATE_PATTERN4, line):
+                date_pattern = DATE_PATTERN4
+                break
+            elif re.match(DATE_PATTERN_PC_DATE, line):
+                date_pattern = DATE_PATTERN_PC_DATE
+                break
+
+    print('found pattern : '+ date_pattern)
+
+    if date_pattern == DATE_PATTERN_PC_DATE:
+        return parse_chat_pc(chat_lines)
+    else:
+        return parse_chat_mobile(chat_lines,date_pattern)
+
+
+def parse_chat_mobile(chat_lines, date_pattern):
+    grouped_chats = defaultdict(list)
+    current_date = None
+    current_time = None
+    for line in chat_lines:
+        # Check for date line
+        date_match = re.match(date_pattern, line)
+        if date_match:
+            if current_date != date_match.group(1):
+                current_date = date_match.group(1)
+                # grouped_chats[current_date].append(current_date)
+
+            if current_time != date_match.group(2):
+                current_time = date_match.group(2)
+                grouped_chats[current_date].append('\n'+current_time)
+
+        elif current_date is None:
+            # pass headers
+            continue
+        else:
+            # continuous line from prvious chat line.
+            grouped_chats[current_date].append(line)
+            continue
+
+
+        # Parse each chat line
+        chat_match = re.match(date_pattern + r',?\s*(.*)\s*[:,]\s*(.*)', line)
+        if chat_match:
+            date_part, time_part, speaker, message = None, None, None, None
+            if len(chat_match.groups()) == 5:
+                # no ampm
+                date_part, am_pm, time_part, speaker, message = chat_match.groups()
+            elif len(chat_match.groups()) == 4:
+                # no ampm
+                date_part, time_part, speaker, message = chat_match.groups()
+            else:
+                # headers
+                continue
+            grouped_chats[current_date].append(f"{speaker}: {message}")
+        
+        
+    result = []
+
+    for date, messages in grouped_chats.items():
+        result.append(f"{date}")
+        result.extend(messages)
+        result.append("")  # for new line between different dates
+    
+    return "\n".join(result)
+
+
+def parse_chat_pc(chat_lines):
+    grouped_chats = defaultdict(list)
+    current_date = None
+    current_time = None
+    for line in chat_lines:
+        # Check for date line
+        date_match = re.match(DATE_PATTERN_PC_DATE, line)
+        date_match_msg = re.match(DATE_PATTERN_PC_MSG, line)
+
+        if date_match:
+            if current_date != date_match.group(1):
+                current_date = date_match.group(1)
+                # continue to next lines
+                continue
+
+        if date_match_msg:
+            if current_time != date_match_msg.group(2):
+                current_time = date_match_msg.group(2)
+                grouped_chats[current_date].append('\n'+current_time)
+
+        elif current_date is None:
+            # pass headers
+            continue
+        else:
+            # continuous line from prvious chat line.
+            grouped_chats[current_date].append(line)
+            continue
+
+
+        # Parse each chat line
+        chat_match = re.match(DATE_PATTERN_PC_MSG + r'\s*(.*)', line)
+        if chat_match:
+            speaker, time_part, message = None, None, None
+            if len(chat_match.groups()) == 4:
+                # ampm
+                speaker, am_pm, time_part, message = chat_match.groups()
+            elif len(chat_match.groups()) == 3:
+                # no ampm
+                speaker, time_part, message = chat_match.groups()
+            else:
+                # headers
+                continue
+            grouped_chats[current_date].append(f"{speaker}: {message}")
+        
+        
+    result = []
+
+    for date, messages in grouped_chats.items():
+        result.append(f"{date}")
+        result.extend(messages)
+        result.append("")  # for new line between different dates
+    
+    return "\n".join(result)
+
+
+def split_text(text, chunk_size=10000, max_chunks=10):
+    # 텍스트를 chunk_size만큼 뒤에서부터 나누기
+    length = len(text)
+    # print("텍스트 길이:"+str(length))
+    chunks = []
+    for i in range(max_chunks):
+        start_index = max(0, length - (i + 1) * chunk_size)
+        end_index = length - i * chunk_size
+        if start_index >= end_index:
+            break
+        chunks.append(text[start_index:end_index])
+        if start_index == 0:
+            break  # 텍스트의 시작까지 도달하면 종료
+    chunks.reverse()  # 뒤에서부터 자른 후 순서를 원래대로 정렬
+    #print("청크:"+str(chunks))
+    return chunks
+
+
+def gpt_request(kakao_chat):
+    client = OpenAI(
+    api_key=api_key,
+    base_url="https://api.upstage.ai/v1/solar"
+    )
+    response = client.chat.completions.create(
+        model="solar-1-mini-chat",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                너는 주요한 대화를 추출하는 역할을 맡았어.
+                아래는 대화 내용이야. 이 중에서 주요한 내용을 추출해줘.
+                각 대화는 누가 이야기했는지 구분되어서 추출되어야 해.
+                """,
+            },
+            {
+                "role": "user",
+                "content": "채팅 내역 : " + kakao_chat,
+            },
+        ],
+    )
+    return response.choices[0].message.content
+
+# 최종 결과 통합때만 gpt-4o 사용
+def aggregate_responses(combined_responses):
+    client = OpenAI(
+    api_key=openai_api_key,
+    )
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                당신은 대화 분석 전문가임. 
+                아래는 과거 대화 내용중 주요 내용을 추출한 것.
+                이를 바탕으로 대화 분석 리포트를 작성.
+                대화 참여자들의 말투, 성격, 추억 등을 분석하는 것이 목표이며, 추억은 최대한 많이 포함시키도록 함.
+                실제 대화 내역이 포함되도록 하고, 대화 내역은 각색하지 않고 그대로 인용. 이는 리포트를 보는 사람들이 실제 자신의 대화 내용을 확인함으로써, 리포트에 더욱 설득되도록 하기 위함임.
+                """,
+            },
+            {
+                "role": "user",
+                "content": combined_responses,
+            },
+            {
+                "role": "system",
+                "content": """
+                각 영역을 ** 등의 기호를 사용해서 구분하지 말고 대괄호안에 주제를 넣는 방식으로 구분.
+                최대한 많은 내용을 포함시키도록 하고, 겹치는 내용은 중복해서 포함시키지 않도록 주의.
+                분석 내용을 뒷받침 하는 자료로 대화 내역을 그대로 사용함.
+                """,
+            },
+        ],
+    )
+    return response.choices[0].message.content
+
+def analyze_past_life2(combined_responses):
+    a = ""
+
+    return a
+
+def analyze_past_life(combined_responses):
+    client = OpenAI(
+    api_key=api_key,
+    base_url="https://api.upstage.ai/v1/solar"
+    )
+    response = client.chat.completions.create(
+        model="solar-1-mini-chat",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                당신은 전생 분석 전문가입니다. 첨부된 채팅 내용 분석 내역을 바탕으로 두 사람이 전생에 어떤 관계였을지 추측하고 이를 재밌는 이야기로 작성해주세요.
+                전생은 사람이 아니라 동식물 혹은 물체일수도 있습니다.
+                
+                대화 내용을 기반으로 하되, 전생이기 때문에 최소 100년 전을 상상하여 관계를 각색하고 밀접한 관계를 만들어주세요.
+                분석이 아닌 이야기 형태로 작성해주세요.
+                이야기는 최소 5문단 이상, 500자 이상으로 작성해주세요. 
+                """,
+            },
+            {
+                "role": "user",
+                "content": "채팅 내역 분석 : " + combined_responses,
+            },
+        ],
+    )
+    return response.choices[0].message.content
+
+def write_poem(combined_responses):
+    client = OpenAI(
+    api_key=api_key,
+    base_url="https://api.upstage.ai/v1/solar"
+    )
+    response = client.chat.completions.create(
+        model="solar-1-mini-chat",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                당신은 시인입니다. 첨부된 채팅 내역 분석을 바탕으로 시를 작성해주세요.
+                직접적으로 두 사람의 관계를 언급하지 않으며, 비유적으로 작성.
+                """,
+            },
+            {
+                "role": "user",
+                "content": "채팅 내역 분석 : " + combined_responses,
+            },
+        ],
+    temperature=0.9,
+    )
+    return response.choices[0].message.content
+
+def create_anniversary(kakao_chat):
+    client = OpenAI(
+    api_key=api_key,
+    base_url="https://api.upstage.ai/v1/solar"
+    )
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                당신은 기념일 생성 전문가입니다. 첨부된 채팅 내역을 바탕으로 두 사람에게 의미 있는 기념일을 만들어주세요.
+                기념일의 날짜(월/일 형식으로), 이유, 이를 위한 이벤트, 그날 나눴던 대화 등을 포함해주세요.
+                그날 나눴던 대화는 각색하지 말고 그대로 인용해주세요.
+                대화와 기념일은 서로 잘 어울리도록 만들어주세요.
+                """,
+            },
+            {
+                "role": "user",
+                "content": "채팅 내역 : " + kakao_chat,
+            },
+        ],
+    )
+    return response.choices[0].message.content
+
+
+def monthly_event(kakao_chat):
+    client = OpenAI(
+    api_key=api_key,
+    base_url="https://api.upstage.ai/v1/solar"
+    )
+    response = client.chat.completions.create(
+        model="solar-1-mini-chat",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                채팅 내역을 바탕으로 각 월의 가장 메인이 되는 이벤트를 명사형으로 추출하고, 그 이벤트에 대한 간략하 설명을 작성.
+                최소 6개 이상의 이벤트를 추출.
+                같은 월을 추출해도 되지만 이벤트 내용은 달라야함.
+                일단위로 추출하지 말고 월단위로 추출. 
+                같은 일자가 추출되면 안됨. (do not extract same date)
+
+                결과 예시 :
+                [n년 m월] : [이벤트명] - [이벤트 설명]
+                """,
+            },
+            {
+                "role": "user",
+                "content": "채팅 내역 : " + kakao_chat,
+            },
+        ],
+    )
+    return response.choices[0].message.content
+
+
+
+
+def make_quiz(combined_responses):
+    client = OpenAI(
+    api_key=api_key,
+    base_url="https://api.upstage.ai/v1/solar"
+    )
+    response = client.chat.completions.create(
+        model="solar-1-mini-chat",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                당신은 퀴즈를 만드는 출제자입니다. 주어진 대화를 보고 두 인물 각각에 대한 정보를 이용한 퀴즈나, 두 인물 간에 있었던
+                사건들에 대한 퀴즈를 만들어서 출제합니다.
+                퀴즈는 총 세개이며, 주관식 1개 객관식 2개를 출제합니다.
+                """,
+            },
+            {
+                "role": "user",
+                "content": "채팅 내역 분석 : " + combined_responses,
+            },
+        ],
+    )
+    return response.choices[0].message.content
+
+def emotion_donut(combined_responses):
+    client = OpenAI(
+    api_key=openai_api_key,
+    )
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                당신은 감정 분석가입니다. 대화 내용을 읽으며 각 단락 별로 어떤 감정이 가장 많이 드러나는지 수량을 분석, 분류해 json 형태로 출력하세요.
+                기쁨이 약간 좀 더 있는 거로 해줘.
+                감정 : ["기쁨", "슬픔", "놀람", "분노", "공포", "혐오", "중립"]
+                
+                output format : 
+                ```json
+                {
+                    "options" :{
+                        "chart": {
+                            "toolbar": {
+                                "show": False
+                            }
+                        },
+
+                        "labels": ["기쁨", "슬픔", "놀람", "분노", "공포", "혐오", "중립"]
+                        ,
+                        "legend": {
+                            "show": True,
+                            "position": "bottom",
+                        }
+                    },
+                    "series" : [(number of each emotions)]
+                }
+                ```
+                """
+                ,
+            },
+            {
+                "role": "user",
+                "content": "채팅 내역 분석 : " + combined_responses,
+            },
+        ],
+    )
+    result =  response.choices[0].message.content
+
+
+    result = '\n'.join(result.strip().splitlines()[1:-1])   # remove extras
+
+    data = json.loads(result)
+
+    # Extract 'options' and 'series' from the dictionary
+    options = data["options"]
+    series = data["series"]
+
+    # Print the resulting Python objects
+    # print("options =", options)
+    # print("series =", series)
+
+    def gen_chart():
+        st_apexcharts(options, series, 'donut', '600', '감정 빈도수')
+
+    return gen_chart
+
+def write_rap_lyric(combined_responses):
+    client = OpenAI(
+    api_key=api_key,
+    base_url="https://api.upstage.ai/v1/solar"
+    )
+    response = client.chat.completions.create(
+        model="solar-1-mini-chat",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+                당신은 국내 최고 힙합 프로듀서이다. 당신은 대화의 내용을 바탕으로 친구와 있었던 특별한 일에 대한 랩 가사를 작성한다.
+                """,
+            },
+            {
+                "role": "user",
+                "content": "채팅 내역 분석 : " + combined_responses,
+            },
+        ],
+    )
+    return response.choices[0].message.content
